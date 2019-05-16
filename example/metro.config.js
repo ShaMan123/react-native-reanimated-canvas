0348d16cf6b28f3a206aecda81ff82fd91486d51
ShaMan123 <shacharnen@gmail.com>
2019-05-16 14:37:30 +0300

    add react-native-gesture-handler

---
+++ example/metro.config.js
@@ -0,0 +1,28 @@
+﻿const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+const pkg = require('./package.json');
+
+//const glob = require('glob-to-regexp');
+
+function getBlacklist() {
+    /*
+    const nodeModuleDirs = [
+        glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+        glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
+        glob(
+            `${path.resolve(
+                __dirname
+            )}/node_modules/*/node_modules/hoist-non-react-statics/*`
+        ),
+    ];
+    */
+    return blacklist(nodeModuleDirs);
+}
+
+module.exports = {
+    resolver: {
+        //blacklistRE: blacklist([path.resolve(__dirname, 'node_modules/react-native-gesture-handler')]),
+        providesModuleNodeModules: Object.keys(pkg.dependencies),
+    },
+    watchFolders: [path.resolve(__dirname, '..')],
+};
