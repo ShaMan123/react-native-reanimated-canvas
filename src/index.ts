c34c58588e5a307cd752f5179b9957f6463e89e2
ShaMan123 <shacharnen@gmail.com>
2019-11-23 16:37:29 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import * as RCanvasModule from './RCanvasModule';
 export { RCanvasModule };
 export * from './hooks';
+export * from './types';
 export { default as RControlledCanvas, generatePathId } from './RCanvasBase';
 export { default } from './RCanvas';
 
