66573d3dc72bf55605009ef0383c005ae5aee117
ShaMan123 <shacharnen@gmail.com>
2019-11-25 00:39:01 +0200

    Update Basic.tsx

--- CanvasExample/App/Basic.tsx
+++ CanvasExample/App/Basic.tsx
@@ -1,10 +1,39 @@
 
-import React from 'react';
+import React, { useRef } from 'react';
 import LegacyCanvas from './LegacyCanvas';
+import { TapGestureHandler, BorderlessButton, LongPressGestureHandler } from 'react-native-gesture-handler';
+import { styles } from './common';
+import Animated from 'react-native-reanimated';
+const { View } = Animated;
 
 
 export default function Basic() {
+  const tap = useRef();
+  const button = useRef();
+  const longPress = useRef();
   return (
-    <LegacyCanvas />
+    <TapGestureHandler
+      ref={tap}
+      waitFor={[button, longPress]}
+      onHandlerStateChange={e => console.log(e.nativeEvent)}
+    //enabled={false}
+    >
+      <View collapsable={false} style={styles.default}>
+        <LongPressGestureHandler
+          onHandlerStateChange={e => console.log('long', e.nativeEvent)}
+          waitFor={button}
+        >
+          <BorderlessButton
+            style={styles.default}
+            ref={button}
+          >
+            <LegacyCanvas canvasStyle={{ flex: 1, backgroundColor: 'red' }} />
+          </BorderlessButton>
+        </LongPressGestureHandler>
+
+      </View>
+
+    </TapGestureHandler>
+
   )
 }
