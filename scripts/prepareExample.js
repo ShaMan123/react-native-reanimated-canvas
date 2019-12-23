c8c26c7ce1432b09feff4508e90223bd7401b919
ShaMan123 <shacharnen@gmail.com>
2019-12-23 14:15:44 +0200

    safe publish/commit example config

---
+++ scripts/prepareExample.js
@@ -0,0 +1,25 @@
+﻿/* tslint:disable: no-console */
+
+const fs = require('fs');
+const _ = require('lodash');
+const path = require('path');
+
+const filePath = path.resolve(__dirname, '..', 'CanvasExample', 'dev.config.json');
+const config = require('../CanvasExample/dev.config.json');
+const defaultConfig = {
+    "useBaseImplementation": false,
+    "useLocalReanimatedModule": false
+}
+
+module.exports = {
+    prepareExample() {
+        fs.writeFileSync(filePath, JSON.stringify(defaultConfig, null, '\t'));
+    },
+
+    restoreExample() {
+        fs.writeFileSync(filePath, JSON.stringify(config, null, '\t'));
+    }
+}
+
+
+
