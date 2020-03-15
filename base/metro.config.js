24c95b8fbdfa9028e2b62b10e34474ccce3fc0ae
ShaMan123 <shacharnen@gmail.com>
2020-03-15 06:33:55 +0200

    drop lodash

--- base/metro.config.js
+++ base/metro.config.js
@@ -1,18 +1,26 @@
 const path = require('path');
 const pkg = require(path.resolve(process.cwd(), './package.json'));
-const _ = require('lodash');
 
 const REANIMATED = 'react-native-reanimated';
 const GESTURE_HANDLER = 'react-native-gesture-handler';
 const ext = '.stub.tsx';
 const extraNodeModules = {};
 
-if (!_.has(pkg.dependencies, REANIMATED)) {
-  _.set(extraNodeModules, REANIMATED, path.format({ dir: __dirname, name: REANIMATED, ext }));
+function has(o, key) {
+  return o[key] !== undefined;
 }
 
-if (!_.has(pkg.dependencies, GESTURE_HANDLER)) {
-  _.set(extraNodeModules, GESTURE_HANDLER, path.format({ dir: __dirname, name: GESTURE_HANDLER, ext }));
+function set(o, key, value) {
+  o[key] = value;
+}
+
+
+if (has(pkg.dependencies, REANIMATED)) {
+  set(extraNodeModules, REANIMATED, path.format({ dir: __dirname, name: REANIMATED, ext }));
+}
+
+if (has(pkg.dependencies, GESTURE_HANDLER)) {
+  set(extraNodeModules, GESTURE_HANDLER, path.format({ dir: __dirname, name: GESTURE_HANDLER, ext }));
 }
 
 module.exports = {
