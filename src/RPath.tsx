e1e0f703c9598e2cac073502d9bbb00613206c7c
ShaMan123 <shacharnen@gmail.com>
2020-04-17 14:49:00 +0300

    hitSlop

--- src/RPath.tsx
+++ src/RPath.tsx
@@ -3,15 +3,17 @@ import { requireNativeComponent, StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { PATH_VIEW_MANAGER } from './RCanvasBaseModule';
 import { RPathProperties } from './types';
-import { generatePathId } from './util';
+import { generatePathId, useHitSlop } from './util';
 
 const RNativePath = Animated.createAnimatedComponent(requireNativeComponent(PATH_VIEW_MANAGER));
 
 function RPathBase(props: RPathProperties, ref: Ref<any>) {
+  const hitSlop = useHitSlop(props.hitSlop);
   return (
     <RNativePath
       {...props}
       id={props.id || generatePathId()}
+      hitSlop={hitSlop}
       style={[StyleSheet.absoluteFill, props.style]}
       ref={ref}
     />
@@ -20,7 +22,8 @@ function RPathBase(props: RPathProperties, ref: Ref<any>) {
 
 const RPath = React.forwardRef(RPathBase);
 RPath.defaultProps = {
-  renderToHardwareTextureAndroid: true
+  renderToHardwareTextureAndroid: true,
+  hitSlop: 20
 }
 RPath.displayName = 'RPath';
 
